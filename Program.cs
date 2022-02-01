50094c08966151576a44a762cd3aaf5d90d75742
Geovane Jorge <geovane.jorge@gmail.com>
2022-02-01 10:35:22 -0300

    Herança

--- Program.cs
+++ Program.cs
@@ -15,16 +15,31 @@ namespace ExemploPOO
             
             //Encapsulamento
             // Valores validos
-            Retangulo r =new Retangulo();
-            r.DefinirMedidas(30, 30);
-            System.Console.WriteLine($"Área:{r.ObterArea()}");
+            // Retangulo r =new Retangulo();
+            // r.DefinirMedidas(30, 30);
+            // System.Console.WriteLine($"Área:{r.ObterArea()}");
 
-            // Valores Invalidos
-            Retangulo r2 =new Retangulo();
-            r.DefinirMedidas(0, 0);
-            System.Console.WriteLine($"Área:{r2.ObterArea()}");
+            // // Valores Invalidos
+            // Retangulo r2 =new Retangulo();
+            // r.DefinirMedidas(0, 0);
+            // System.Console.WriteLine($"Área:{r2.ObterArea()}");
 
 
+                // Aula de Herança
+                // Aluno p1=new Aluno();
+                // p1.Nome="Bob";
+                // p1.Idade=20;
+                // p1.Documento="123456";
+                // p1.Apresentar();
+
+
+              //Aula de Herança
+                Professor p1=new Professor();
+                p1.Salario=2000;
+                p1.Nome="Bob";
+                p1.Idade=20;
+                p1.Documento="123456";
+                p1.Apresentar();
         }
     }
 
