cb3147bab2968899fca0af64befdd26f34da7281
Geovane Jorge <geovane.jorge@gmail.com>
2022-02-02 15:22:35 -0300

    Aula de Polimorfismo em tempo de execução virtual override

--- Models/Professor.cs
+++ Models/Professor.cs
@@ -1,7 +1,12 @@
 namespace ExemploPOO.Models
 {
-    public class Professor :Pessoa
+    public class Professor : Pessoa
     {
         public double Salario { get; set; }
+        public override void Apresentar()
+        {
+            Console.WriteLine($"Olá, meu nome é {Nome} e sou professor e ganho {Salario}");
+        }
+        
     }
 }
--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ namespace ExemploPOO
             // p1.Idade=20;
             // p1.Apresentar();
             
-            
+            //====================================
             //Encapsulamento
             // Valores validos
             // Retangulo r =new Retangulo();
@@ -24,7 +24,7 @@ namespace ExemploPOO
             // r.DefinirMedidas(0, 0);
             // System.Console.WriteLine($"Área:{r2.ObterArea()}");
 
-
+                //=====================
                 // Aula de Herança
                 // Aluno p1=new Aluno();
                 // p1.Nome="Bob";
@@ -32,14 +32,38 @@ namespace ExemploPOO
                 // p1.Documento="123456";
                 // p1.Apresentar();
 
+                // Aula de Herança
+                // Professor p1=new Professor();
+                // p1.Salario=2000;
+                // p1.Nome="Bob";
+                // p1.Idade=20;
+                // p1.Documento="123456";
+                // p1.Apresentar();
 
-              //Aula de Herança
-                Professor p1=new Professor();
-                p1.Salario=2000;
-                p1.Nome="Bob";
-                p1.Idade=20;
-                p1.Documento="123456";
-                p1.Apresentar();
+                // Aula de Herança
+                // Professor p1=new Professor();
+                // p1.Salario=2000;
+                // p1.Nome="Bob";
+                // p1.Idade=20;
+                // p1.Documento="123456";
+                // p1.Apresentar();
+                //====================================
+                // Aula de Polimorfismo em tempo de execução virtual override 
+                // Aluno p1=new Aluno();
+                // p1.Nome="Bob";
+                // p1.Idade=20;
+                // p1.Documento="123456";
+                // p1.Nota=10;
+                // p1.Apresentar();
+
+                // Aula de Polimorfismo em tempo de execução virtual override 
+                // Professor p1=new Professor();
+                // p1.Salario=2000;
+                // p1.Nome="Bob";
+                // p1.Idade=20;
+                // p1.Documento="123456";
+                // p1.Apresentar();
+
         }
     }
 
